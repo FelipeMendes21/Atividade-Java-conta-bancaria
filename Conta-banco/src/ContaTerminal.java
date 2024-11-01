eb5d9205f2c4847eff098fd427ea3a57dc152970
Seu Nome <seu-email@example.com>
2024-11-01 08:13:40 -0300

    sistema-feito

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        try (//criando o objeto scanner
+                Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+
+            //Nome do cliente
+            System.out.println("Digite seu nome");
+            String nomeCliente = scanner.next();
+            
+            //agencia
+            System.out.println("Por favor, digite o número da agencia");
+            String agencia = scanner.next();
+
+            //Usuario
+            System.out.println("Numero");
+            int numero = scanner.nextInt();
+            
+
+            //saldo
+            
+            double saldo = 237.48;
+
+            
+
+
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque" );
+        }
     }
 }
